e2950c5fc45f6dbd1ac2b1bd2eb2784a92443470
anastasiam <moiseevaanastasia.98@gmail.com>
2019-12-14 06:26:04 +0300

    add filter item layout and tool bar

--- app/src/main/java/com/example/photoai/FragmentActivity.kt
+++ app/src/main/java/com/example/photoai/FragmentActivity.kt
@@ -1,6 +1,9 @@
 package com.example.photoai
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.photoai.fragments.MainFragment
 import com.example.photoai.router.Router
@@ -14,8 +17,26 @@ class FragmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.container_layout)
 
+        createToolBar()
+
         router = Router(this, R.id.fragment_container)
 
         if (savedInstanceState == null) router.navigateTo(false, ::MainFragment)
+    }
+
+    fun createToolBar(){
+        val textView = findViewById(R.id.txt_photo_ai) as TextView
+
+        val paint = textView.getPaint()
+        val width = paint.measureText(R.string.app_name.toString())
+
+        val textShader = LinearGradient(
+            0f, 0f, width, textView.getTextSize(),
+            intArrayOf(
+                getResources().getColor(R.color.startGradientColor),
+                getResources().getColor(R.color.finishGradientColor)
+            ), null, Shader.TileMode.CLAMP
+        )
+        textView.getPaint().setShader(textShader)
     }
 }
--- app/src/main/java/com/example/photoai/fragments/FilterItem.kt
+++ app/src/main/java/com/example/photoai/fragments/FilterItem.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.photoai.R
 import com.example.photoai.router.Router
@@ -13,8 +15,13 @@ class FilterItem : Fragment() {
 
     private lateinit var router : Router
 
+    private var position = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        position = this.arguments!!.getInt(message)
 
         router = Router(requireActivity(), R.id.fragment_container)
     }
@@ -23,7 +30,9 @@ class FilterItem : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.load_img_layout, container, false)
+        val view = inflater.inflate(R.layout.filter_item_layout, container, false)
+        var photo = view?.findViewById<ImageView>(R.id.img_gradient)!!
+        val plusButton = view.findViewById<ImageButton>(R.id.img_btn_plus)
         return view
     }
 }
